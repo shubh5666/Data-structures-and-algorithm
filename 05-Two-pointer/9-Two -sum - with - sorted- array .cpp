6289ad4170985f723495fc62aa834d7a92fbdba2
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2025-12-28 12:26:07 +0530

    28th Dec 2025

---
+++ 05-Two-pointer/9-Two -sum - with - sorted- array .cpp
@@ -0,0 +1,27 @@
+// submission link :- https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        int left = 0 , right = numbers.size()-1;
+
+        while(left<right){
+            if( numbers[left] + numbers[right] == target){
+                return {left + 1,right + 1};
+
+            
+            }
+
+            else if ( numbers[left] + numbers[right] < target){
+                left ++;
+            }
+
+            else{
+                right--;
+            }
+        }
+
+        return { };
+    }
+};

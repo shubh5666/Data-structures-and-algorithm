7f7a453021fe0471e8a816aafc92d7a1586e57fb
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2025-12-28 11:45:55 +0530

    28th Dec 2025

---
+++ 05-Two-pointer/8-Count-pair-sum-less-than-target.cpp
@@ -0,0 +1,25 @@
+//Submission link :- https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+
+        sort(nums.begin(),nums.end());
+        int count = 0;
+        
+        int  left = 0, right  = nums.size()-1;
+        while(left < right ){
+            if(nums[left ] + nums[right ] < target){
+                count+= right-left;
+                left++;
+               
+            }
+          
+
+            else{
+                right--;
+            }
+        }
+         return count;
+    }
+};

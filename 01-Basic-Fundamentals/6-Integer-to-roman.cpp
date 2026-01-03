91906ca00635e1246c150f75ae343b1d21a48743
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-01-03 18:56:08 +0530

    3th Dec 2025

---
+++ 01-Basic-Fundamentals/6-Integer-to-roman.cpp
@@ -0,0 +1,26 @@
+// submission link:- https://leetcode.com/problems/integer-to-roman/submissions/1873090484/
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int,string>>roman = {
+            {1000,"M"}, {900,"CM"}, {500,"D"}, {400,"CD"},
+            {100,"C"}, {90,"XC"}, {50,"L"}, {40,"XL"},
+            {10,"X"}, {9,"IX"}, {5,"V"}, {4,"IV"}, {1,"I"}
+        };
+
+
+        string ans = "";
+
+        for(int i = 0 ;i<roman.size();i++){
+            while(num>= roman[i].first){
+                ans+= roman[i].second;
+                num -=roman[i].first;
+            }
+
+        }
+
+        return ans;
+
+            }
+};

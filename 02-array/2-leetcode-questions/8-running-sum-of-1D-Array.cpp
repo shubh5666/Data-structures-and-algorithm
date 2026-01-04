d2ca8d48030b578bac81cd113d4839c9082460ad
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-01-04 23:19:47 +0530

    4th Jan 2026

---
+++ 02-array/2-leetcode-questions/8-running-sum-of-1D-Array.cpp
@@ -0,0 +1,12 @@
+// submission link:- https://leetcode.com/problems/running-sum-of-1d-array/submissions/1874504382/
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+         for (int i = 1; i < nums.size(); i++) {
+            nums[i] = nums[i] + nums[i - 1];
+         }
+
+         return nums;
+    }
+};

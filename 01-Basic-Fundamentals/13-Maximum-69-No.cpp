830f9694ee6c7a14ecac60c24138b86f441142c1
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-01-11 19:13:01 +0530

    11th Jan 2026

---
+++ 01-Basic-Fundamentals/13-Maximum-69-No.cpp
@@ -0,0 +1,17 @@
+// Submission link:- https://leetcode.com/problems/maximum-69-number/submissions/1881816583/
+
+class Solution {
+public:
+    int maximum69Number(int num) {
+        string s = to_string(num);
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '6') {
+                s[i] = '9';
+                break;
+            }
+        }
+
+        return stoi(s);
+    }
+};

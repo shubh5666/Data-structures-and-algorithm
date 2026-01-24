07eb9647adf2f6ae90a3b1b5bc99a98c1fd10e9c
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-01-24 21:35:18 +0530

    24th Jan 2026

---
+++ 01-Basic-Fundamentals/26-Plus-one.cpp
@@ -0,0 +1,20 @@
+// Submission link:- https://leetcode.com/problems/plus-one/submissions/1895494199/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};

642ad896de7a374efe3dd749ab019294c4002571
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-01-31 20:24:11 +0530

    31th Jan 2026

---
+++ 02-array/2-leetcode-questions/11-Hw-mny-no-smaller-than-curr-no.cpp
@@ -0,0 +1,22 @@
+// Submission link:- https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/submissions/1903134996/
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+
+            ans.push_back(count);
+        }
+
+        return ans;
+    }
+};

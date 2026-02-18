7e03e5ed4da61d628be51b1b034a6dd4b039c18f
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-02-18 22:56:56 +0530

    18th Feb 2026

--- 08-stack/1-basic/3-implementation-of-stack-using-vector.cpp
+++ 08-stack/1-basic/3-implementation-of-stack-using-vector.cpp
@@ -1,36 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Stack{
-    vector<int>v;
-    public:
+class MyStack {
+    vector<int> v;
+
+public:
 
     void push(int val){
-         v.push_back(val);
+        v.push_back(val);
     }
 
     void pop(){
-       v.pop_back();
+        if(v.size() == 0){
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        v.pop_back();
     }
 
-    int top() {
-         return v[v.size()-1];
-
+    int top(){
+        if(v.size() == 0){
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return v[v.size() - 1];
     }
+
     bool empty(){
         return v.size() == 0;
     }
 };
 
-int main(){
-    Stack s;
+void traverse(MyStack s){
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+}
 
+int main(){
+    MyStack s;
+
+    s.push(10);
     s.push(20);
     s.push(30);
     s.push(40);
-    while(!s.empty()){
-        cout<<s.top()<< " ";
-        s.pop();
-    }
-    cout<<endl;
+
+    traverse(s);
 }

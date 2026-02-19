e7ec2183aae493ce87df2b2aa05ad0d50879f19b
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-02-19 18:18:30 +0530

    19th Feb 2026

--- 08-stack/2-intermediate/1-next-greater-element.cpp
+++ 08-stack/2-intermediate/1-next-greater-element.cpp
@@ -6,39 +6,38 @@ using namespace std;
 int main(){
    vector<int>v={4,20,56,89,21};
    stack<int>st;
-   vector<int>ans; // empty vector created to store the next greater element of each veactor(v).
-   for(int i = v.size()-1;i>=0;i--){
-      if(st.size()==0){
+  vector<int>ans;
+
+    for(int i = v.size()-1;i>=0;i--){
+      if(st.size() == 0){
          ans.push_back(-1);
       }
-      else if(st.size()>0 && st.top()>=v[i]){
-//check stack is not empty and stack is greater than or equal to current element.
-           ans.push_back(st.top());
 
-      }
-      else if(st.size()>0 && st.top()<=v[i]){
-         while(st.size()>0 && st.top()<=v[i]){
+       else if(st.size() > 0 && st.top() > v[i]){
+         ans.push_back(st.top());
+       }
+
+       else if( st.size() > 0 && st.top() <= v[i]){
+         while(st.size() > 0 && st.top() <= v[i]){
             st.pop();
-//if the stack's top element is smaller than or equal to the current element, 
-//we pop elements from the stack until we find a greater element.
          }
-         if(st.size()==0){
+         if(st.size() ==0){
             ans.push_back(-1);
          }
+
          else{
             ans.push_back(st.top());
          }
-      }
-        st.push(v[i]);
-// push the current element on the stack for future comparison.
-   }
 
-   for(int i=ans.size()-1;i>=0;i--){
+       }
+
+       st.push(v[i]);
+
+    }
+    reverse(ans.begin() , ans.end());
+
+    for(int i=0;i<ans.size();i++){
       cout<<ans[i]<<" ";
-   }
-   cout<<endl;
- 
+    }
+
 }
-
-
-

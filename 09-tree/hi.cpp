d8a2729d261db63a2b2574e481f81ba9c7575a26
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-03-21 15:44:59 +0530

    21th Mar, 2026

--- 09-tree/hi.cpp
+++ 09-tree/hi.cpp
@@ -1,23 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-  
-struct Node{
+class Node
+{
+public:
     int data;
     Node *left;
     Node *right;
 
-    Node(int val){
-        data=val;
-        left=right=NULL;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
     }
 };
 
+static int idx = -1;
+Node *buildTree(vector<int> &preorder)
+{
+    idx++;
 
+    if (preorder[idx] == -1)
+    {
+        return NULL;
+    }
 
-int main(){
+    Node *root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
 
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-     root->left->right = new Node(5);
+    return root;
+}
+
+int main()
+{
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node* root = buildTree(preorder);
+    cout << root->data << endl;
 }
